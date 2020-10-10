bf0f9235f76a9a08468066684dd1be0cd5319034
Moshe Levi <mosh92@gmail.com>
2020-10-10 17:24:36 +0300

    Update Song.py

--- Song.py
+++ Song.py
@@ -15,6 +15,9 @@ class Song:
         self.song_name = self.get_song_name()
         self.media = self.get_media()
 
+#     For each song the ctor call this function twice, think on a way to call this only once.
+# in most of the languages you have private and public fields, but in python you dont have, so the developer started a convention that each field/function that is private should start with _, for example 
+# _get_song_url. please update all your project depend on this convention (you can rename stuff by Shift + F6/F2 )
     def get_song_url(self):
         query_string = urllib.parse.urlencode({"search_query": self.artist})
         html_content = urllib.request.urlopen("http://www.youtube.com/results?" + query_string)
@@ -27,6 +30,7 @@ class Song:
         request = requests.get(url)
         s = BeautifulSoup(request.text, "html.parser")
         song_name = s.find("title").text.replace("\n", "").lower()
+# really long line, hard to read, try split it into more readable lines. 
         song_name = re.sub(r'(youtube)', '', re.sub(r'\(.*\)', '', song_name)) \
             .replace('-', '').replace(self.artist.lower(), "").strip().lower()
         return song_name
